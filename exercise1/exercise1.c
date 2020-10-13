052868983d44f24a1c9135274a2db177fdf14d4e
ARAmarodde3 <72499357+ARAmarodde3@users.noreply.github.com>
2020-10-13 18:12:24 +0200

    Version 1

--- exercise1/exercise1.c
+++ exercise1/exercise1.c
@@ -1,6 +1,17 @@
 
-/* Here, you must include the required libraries */
+#include <stdio.h>
+#include <math.h>
 
 void main(){
-	/* Here, you must write the source code */
+	float gd, x1, x2, y1, y2;
+	
+	printf("Please, enter the coordinates of point x (x1 x2): ");
+	scanf("%f%f", &x1, &x2);
+	
+	printf("Please, enter the coordinates of point y (y1 y2): ");
+	scanf("%f%f", &y1, &y2);
+	
+	gd = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	
+	printf("The geometrical distance between x(%f,%f) and y(%f,%f) is: %f", x1, x2, y1, y2, gd);
 }
