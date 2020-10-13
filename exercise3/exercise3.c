349285e36639a22801f825ca31f3bb67e686e3ef
ARAmarodde3 <72499357+ARAmarodde3@users.noreply.github.com>
2020-10-13 19:54:32 +0200

    Version 1

--- exercise3/exercise3.c
+++ exercise3/exercise3.c
@@ -1,6 +1,15 @@
 
-/* Here, you must include the required libraries */
+#include <stdio.h>
 
 void main(){
-	/* Here, you must write the source code */
+	int age, secret_number, magic_number, x;
+	
+	printf("Introduce Magic Number: ");
+	scanf("%d", &magic_number);
+	
+	secret_number = magic_number - ((magic_number / 10) * 10);
+	
+	age = ((magic_number + secret_number * 9) / 10);
+	
+	printf("Your afe is %d y the secret number is %d.", age, secret_number);
 }
